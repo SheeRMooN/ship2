fd2cbb24bfabeee577a91ad94d04a1793e50e74d
SheeRMooN <sheermood@gmail.com>
2021-04-22 20:25:58 +0300

    up

--- ship/src/main/java/com/example/ship/repository/ShipRepo.java
+++ ship/src/main/java/com/example/ship/repository/ShipRepo.java
@@ -4,7 +4,6 @@ import com.example.ship.model.Ship;
 import com.example.ship.model.ShipId;
 import com.example.ship.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
